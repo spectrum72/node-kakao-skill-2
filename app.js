f635efab70e0d938dfb735f1f094203f0aebe798
spectrum72 <120698960+spectrum72@users.noreply.github.com>
2024-09-06 23:31:48 +0900

    나는김시습이다

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+/*app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'njk');
 nunjucks.configure('views', { 
   express: app,
   watch: true,
-});
+});*/
 
 app.use(logger('dev'));
 app.use(express.json());
